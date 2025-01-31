287a36bbd88360cdb6b438a15abe22acfda9b245
yusuffadilahr <yusuffadilah58@gmail.com>
2025-01-31 14:56:16 +0700

    fix: instance axios change

--- apps/web/src/utils/axiosInstance/index.ts
+++ apps/web/src/utils/axiosInstance/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://clean-n-click-production.up.railway.app/api'
 })
 
 export { instance }
